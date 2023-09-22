0843563188835f7da822ca0d455c76374c1f4e56
Gustavo Acosta <gustavoacosta3308@gmail.com>
2023-09-22 00:24:11 -0300

    arreglos

--- main.js
+++ main.js
@@ -1,27 +1,26 @@
-function tarea(nombre, estado, descripcion){
+function Tarea(nombre, estado, descripcion) {
     this.nombre = nombre;
-    this.estado = estado;
     this.descripcion = descripcion;
+    this.estado = estado;
 }
 
+const tareas = [];
 
-const tareas = []
-
-function agregarTarea () {
+function agregarTarea() {
     const nombre = document.getElementById('tareaNombre').value;
     const descripcion = document.getElementById('tareaDescripcion').value;
-    const nuevaTarea = new tarea(nombre, descripcion, 'pendiente');
+    const nuevaTarea = new Tarea(nombre, 'pendiente', descripcion); // Corrige el orden de los parámetros
     tareas.push(nuevaTarea);
     actualizarListaTareas();
 }
+
 function eliminarTarea() {
     const nombre = document.getElementById('tareaNombre').value;
     const tareaIndex = tareas.findIndex(tarea => tarea.nombre === nombre);
-    if (tareaIndex !== -1){
-        tareas.splice(tareaIndex, 1)
+    if (tareaIndex !== -1) {
+        tareas.splice(tareaIndex, 1);
         actualizarListaTareas();
-    }
-    else {
+    } else {
         alert(`No se encontró ninguna tarea con el nombre "${nombre}".`);
     }
 }
@@ -29,27 +28,27 @@ function eliminarTarea() {
 function actualizarEstadoTarea() {
     const nombre = document.getElementById('tareaNombre').value;
     const nuevoEstado = document.getElementById('tareaEstado').value;
-    const tareaa = tareas.find(tarea => tarea.nombre === nombre);
-    if (tareaa) {
-        tareaa.estado = nuevoEstado;
-        actualizarEstadoTarea();
+    const tarea = tareas.find(tarea => tarea.nombre === nombre);
+    if (tarea) {
+        tarea.estado = nuevoEstado;
+        actualizarListaTareas(); // Cambia el nombre de la función llamada
     } else {
         alert(`No se encontró ninguna tarea con el nombre "${nombre}".`);
     }
 }
 
 function actualizarListaTareas() {
-        const tareasList = document.getElementById('tareas');
-        tareasList.innerHTML = ''; 
-        tareas.forEach(tarea => {
-            const listItem = document.createElement('li');
-            listItem.textContent = `Nombre: ${tarea.nombre}, Descripción: ${tarea.nombre}, Estado: ${tarea.estado}`;
-            tareasList.appendChild(listItem);
-        });
-    }
+    const tareasList = document.getElementById('tareas');
+    tareasList.innerHTML = '';
+    tareas.forEach(tarea => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `Nombre: ${tarea.nombre}, Descripción: ${tarea.descripcion}, Estado: ${tarea.estado}`; // Corrige la cadena de texto
+        tareasList.appendChild(listItem);
+    });
+}
 
 function buscarTareaPorNombre() {
-    const nombre = document.getElementById('trabajoNombre').value;
+    const nombre = document.getElementById('tareaNombre').value; // Corrige el ID
     const tareaEncontrada = tareas.filter(tarea => tarea.nombre === nombre);
     if (tareaEncontrada.length > 0) {
         alert(`Tareas encontradas con el nombre "${nombre}":\n${JSON.stringify(tareaEncontrada)}`);
@@ -59,7 +58,7 @@ function buscarTareaPorNombre() {
 }
 
 function filtrarTareasPorEstado() {
-    const estado = document.getElementById('tareaNombre').value;
+    const estado = document.getElementById('tareaEstado').value; // Corrige el ID
     const tareasFiltradas = tareas.filter(tarea => tarea.estado === estado);
     if (tareasFiltradas.length > 0) {
         alert(`Tareas con estado "${estado}":\n${JSON.stringify(tareasFiltradas)}`);
